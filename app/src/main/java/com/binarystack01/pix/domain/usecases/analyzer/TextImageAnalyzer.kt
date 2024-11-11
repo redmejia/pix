57ae17b387081ee5bad599fb25eeb6298ac7222b
redmejia <reynaldoemejia@gmail.com>
2024-11-10 20:48:17 -0800

    Refactor analyze image function

--- app/src/main/java/com/binarystack01/pix/domain/usecases/analyzer/TextImageAnalyzer.kt
+++ app/src/main/java/com/binarystack01/pix/domain/usecases/analyzer/TextImageAnalyzer.kt
@@ -6,6 +6,7 @@ import androidx.camera.core.ExperimentalGetImage
 import androidx.camera.core.ImageAnalysis
 import androidx.camera.core.ImageProxy
 import com.google.mlkit.vision.common.InputImage
+import com.google.mlkit.vision.text.Text
 import com.google.mlkit.vision.text.TextRecognition
 import com.google.mlkit.vision.text.latin.TextRecognizerOptions
 
@@ -16,6 +17,24 @@ class TextImageAnalyzer(
 
     private val textRecognizer = TextRecognition.getClient(TextRecognizerOptions.DEFAULT_OPTIONS)
 
+    private fun detectedTextBuilder(visionText: Text): String {
+
+        val outputString = StringBuilder()
+
+        for (block in visionText.textBlocks) {
+            outputString.append("\n")
+            for (line in block.lines) {
+                outputString.append(line.text)
+                outputString.append(" ")
+            }
+        }
+
+        outputString.append("\n")
+
+        return outputString.toString()
+
+    }
+
     @OptIn(ExperimentalGetImage::class)
     override fun analyze(imageProxy: ImageProxy) {
 
@@ -24,14 +43,15 @@ class TextImageAnalyzer(
         if (image != null) {
             val inputImage =
                 InputImage.fromMediaImage(image, imageProxy.imageInfo.rotationDegrees)
-
             textRecognizer.process(inputImage)
                 .addOnSuccessListener { visionText ->
 
                     val text: String = visionText.text
-
                     if (text.isNotBlank()) {
-                        onDetectedText(text)
+                        val outputText = detectedTextBuilder(visionText)
+                        onDetectedText(outputText)
+                    } else {
+                        return@addOnSuccessListener
                     }
                 }
                 .addOnFailureListener { e ->
