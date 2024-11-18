2ad8921b46dc43ab57e91d6792b5428d71609691
redmejia <reynaldoemejia@gmail.com>
2024-11-17 20:42:03 -0800

    implementing zoom in and zoom out

--- app/src/main/java/com/binarystack01/pix/presentation/ui/screens/gallery/Gallery.kt
+++ app/src/main/java/com/binarystack01/pix/presentation/ui/screens/gallery/Gallery.kt
@@ -1,7 +1,9 @@
 package com.binarystack01.pix.presentation.ui.screens.gallery
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.detectTransformGestures
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -14,13 +16,17 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.unit.dp
 import com.binarystack01.pix.presentation.viewmodel.captureviewmodel.CaptureViewModel
+import com.binarystack01.pix.ui.theme.BlackPrimary40
 
 
 @Composable
@@ -29,6 +35,10 @@ fun Gallery(
 ) {
     val photoState by captureViewModel.photoState.collectAsState()
     val selectPhotoId = remember { mutableStateOf("") }
+
+    val scale = remember { mutableFloatStateOf(1f) }
+    val offsetX = remember { mutableFloatStateOf(0f) }
+    val offsetY = remember { mutableFloatStateOf(0f) }
 
     LaunchedEffect(selectPhotoId.value) {
         if (selectPhotoId.value.isNotEmpty()) {
@@ -68,16 +78,32 @@ fun Gallery(
                     modifier = Modifier.fillMaxSize()
                 )
                 // test box
-                Box (
-                    modifier = Modifier.matchParentSize(),
-                ) {
-                    photoState.photo?.let { photo ->
-
-                        Image(
-                            modifier = Modifier.fillMaxSize(),
-                            bitmap = photo.photo.asImageBitmap(),
-                            contentDescription = null
-                        )
+                if (photoState.photo?.id?.isNotEmpty() == true) {
+                    Box(
+                        modifier = Modifier
+                            .matchParentSize()
+                            .background(color = BlackPrimary40)
+                    ) {
+                        photoState.photo?.let { photo ->
+                            Image(
+                                modifier = Modifier
+                                    .fillMaxSize()
+                                    .graphicsLayer {
+                                        scaleX = scale.floatValue
+                                        scaleY = scale.floatValue
+                                        translationX = offsetX.floatValue
+                                        translationY = offsetY.floatValue
+                                    }
+                                    .pointerInput(Unit) {
+                                        detectTransformGestures { _, _, zoom, _ ->
+                                            scale.floatValue =
+                                                (scale.floatValue * zoom).coerceIn(1f, 3f)
+                                        }
+                                    },
+                                bitmap = photo.photo.asImageBitmap(),
+                                contentDescription = null
+                            )
+                        }
                     }
                 }
             }
