55c78bbf0b2bec00701bdf53ae469110e562a7de
redmejia <reynaldoemejia@gmail.com>
2024-11-17 23:34:17 -0800

    Refactor Control default buttons colors

--- app/src/main/java/com/binarystack01/pix/presentation/ui/components/actionbuttons/ControlButton.kt
+++ app/src/main/java/com/binarystack01/pix/presentation/ui/components/actionbuttons/ControlButton.kt
@@ -3,6 +3,7 @@ package com.binarystack01.pix.presentation.ui.components.actionbuttons
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.IconButtonColors
+import androidx.compose.material3.IconButtonDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -13,16 +14,18 @@ import com.binarystack01.pix.R
 @Composable
 fun ControlButton(
     onClick: () -> Unit = {},
-    painter: Int = R.drawable.rotate_camera,
-) {
-    IconButton(
-        onClick = { onClick() },
-        colors = IconButtonColors(
+    colors: IconButtonColors = IconButtonDefaults
+        .iconButtonColors(
             containerColor = BlackPrimary95,
             contentColor = WhitePrimary0,
             disabledContentColor = WhitePrimary0,
             disabledContainerColor = BlackPrimary95
-        )
+        ),
+    painter: Int = R.drawable.rotate_camera,
+) {
+    IconButton(
+        onClick = { onClick() },
+        colors = colors,
     ) {
         Icon(painter = painterResource(id = painter), contentDescription = "action")
     }
